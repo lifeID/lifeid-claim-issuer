b61c279a2de368448cd71dcf7930e9a83792ed1c
Seth Bailey <seth.h.bailey@gmail.com>
2018-06-08 11:09:15 -0700

    fixed left-pad error

--- src/services/codeService.ts
+++ src/services/codeService.ts
@@ -1,9 +1,11 @@
 import * as leftPad from "left-pad";
-function generateCode(max) {
-  return leftPad(_getRandomInt(max), max.length, 0);
+
+function generateCode(max: number): string {
+  const maxLength = max.toString().length;
+  return leftPad(_getRandomInt(max), maxLength, 0);
 }
 
-function _getRandomInt(max) {
+function _getRandomInt(max: number): number {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
